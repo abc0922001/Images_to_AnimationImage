221554c0036359a47176c03b45a1057829d46588
abc0922001 <abc0922001@hotmail.com>
2021-04-14 23:16:19 +0800

    modify bmp to jpg

--- make_image_to_gif.ps1
+++ make_image_to_gif.ps1
@@ -4,7 +4,7 @@
 
 #rename
 cd make
-dir *.bmp | %{$x=0} {Rename-Item $_ -NewName "Base$($x.tostring('000000')).bmp"; $x++ }
+dir *.jpg | %{$x=0} {Rename-Item $_ -NewName "Base$($x.tostring('000000')).jpg"; $x++ }
 
 #make to webp
 #1   800x450
@@ -12,4 +12,4 @@ dir *.bmp | %{$x=0} {Rename-Item $_ -NewName "Base$($x.tostring('000000')).bmp";
 #0.7 560x315
 cd..
 $Today=Get-Date -Format "yyyyMMddHHmmss"
-ffmpeg.exe -f image2 -i ".\make\Base%6d.bmp" -filter_complex "fps=30" -vsync 0 -loop 0 -s 800x450 -y .\$Today.gif
+ffmpeg.exe -f image2 -i ".\make\Base%6d.jpg" -filter_complex "fps=30" -vsync 0 -loop 0 -s 800x450 -y .\$Today.gif
