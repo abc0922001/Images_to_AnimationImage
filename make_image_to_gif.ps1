0888c8f3218f57bb783d99cca16c56b3f229848f
abc0922001 <abc0922001@hotmail.com>
2021-04-15 20:57:27 +0800

    modify to -vf "scale" from -s

--- make_image_to_gif.ps1
+++ make_image_to_gif.ps1
@@ -7,9 +7,6 @@ cd make
 dir *.jpg | %{$x=0} {Rename-Item $_ -NewName "Base$($x.tostring('000000')).jpg"; $x++ }
 
 #make to webp
-#1   800x450
-#0.8 640x360
-#0.7 560x315
 cd..
 $Today=Get-Date -Format "yyyyMMddHHmmss"
-ffmpeg.exe -f image2 -framerate 15 -i ".\make\Base%6d.jpg" -vsync 0 -loop 0 -s 800x450 -y .\$Today.gif
+ffmpeg.exe -f image2 -framerate 15 -i ".\make\Base%6d.jpg" -vsync 0 -loop 0 -vf "scale=iw/2.4:ih/2.4" -y .\$Today.gif
