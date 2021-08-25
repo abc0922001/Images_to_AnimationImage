29c0c0cde7d783c8f64088ae7963be92569a8bca
abc0922001 <abc0922001@users.noreply.github.com>
2021-08-25 15:48:00 +0800

    change to VideoToAnimationImage

--- make_image_to_gif.sh
+++ make_image_to_gif.sh
@@ -1,8 +1,8 @@
-input=".\make\Base%6d.jpg"
 today=`date +%Y%m%d%H%M%S`
-filters="scale=350:-1:flags=lanczos"
-pale="split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"
-#make to webp
+starttime=1
+time=2.5
+size=480
+giffps=30
 
-ffmpeg -f image2 -framerate 30 -i "$input" -vf "$filters,$pale" -loop 0 -r 30 ./${today}.gif
+./ffmpeg.exe -ss ${starttime} -t ${time} -i output.mp4 -filter_complex "[0:v] fps=$giffps,scale=w=$size:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" ${today}.gif
 #read -n 1 -p "Press any key to continue..."
