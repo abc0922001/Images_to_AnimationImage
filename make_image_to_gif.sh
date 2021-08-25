7a1a60fbe06b296eabf1c3ab9c5121966cb156b2
abc0922001 <abc0922001@hotmail.com>
2021-08-25 20:09:24 +0800

    optimize gif quality

--- make_image_to_gif.sh
+++ make_image_to_gif.sh
@@ -2,5 +2,5 @@ frame=30
 giffps=30
 size=480
 today=`date +%Y%m%d%H%M%S`
-ffmpeg -f image2 -framerate ${frame} -i ".\make\Base%6d.jpg" -vf "scale=$size:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 -r ${giffps} ./${today}.gif
+ffmpeg -f image2 -framerate ${frame} -i ".\make\Base%6d.jpg" -filter_complex "[0:v] fps=$giffps,scale=w=$size:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" -loop 0 -r ${giffps} ./${today}.gif
 #read -n 1 -p "Press any key to continue..."
