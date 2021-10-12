39405d6b0504ef869aefe1716fc6efa4b68fbde9
abc0922001 <abc0922001@hotmail.com>
2021-10-12 22:12:33 +0800

    Code refactoring make_image_to_gif.sh

--- make_image_to_gif.sh
+++ make_image_to_gif.sh
@@ -2,5 +2,6 @@ frame=30
 giffps=30
 size=480
 today=`date +%Y%m%d%H%M%S`
+
 ffmpeg -f image2 -framerate ${frame} -i ".\make\Base%6d.jpg" -vf "scale=$size:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 -r ${giffps} ./${today}.gif
 #read -n 1 -p "Press any key to continue..."
