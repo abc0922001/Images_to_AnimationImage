d4c9f79d50cae0ec349b2044bd912a8202b8e9d3
abc0922001 <abc0922001@hotmail.com>
2021-12-29 21:27:19 +0800

    optimize gif quality : paletteuse
    paletteuse=dither=bayer:bayer_scale=3

--- 2make_image_to_gif.sh
+++ 2make_image_to_gif.sh
@@ -7,5 +7,5 @@ frame=30
 giffps=${frame}
 today=`date +%Y%m%d%H%M%S`
 
-".\ffmpeg.exe" -f image2 -framerate ${frame} -i ".\make\Base%6d.jpg" -vf "crop=${w_cut}:${h_cut}:${x_cutpoint}:${y_cutpoint},scale=$size:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 -r ${giffps} ./${today}.gif
+".\ffmpeg.exe" -f image2 -framerate ${frame} -i ".\make\Base%6d.jpg" -vf "crop=${w_cut}:${h_cut}:${x_cutpoint}:${y_cutpoint},scale=$size:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3" -loop 0 -r ${giffps} ./${today}.gif
 #read -n 1 -p "Press any key to continue..."
