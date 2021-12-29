0b2ad78bdc1f83183af7e56129fcc4e7512d54e5
abc0922001 <abc0922001@hotmail.com>
2021-12-29 19:39:11 +0800

    Revert "optimize gif quality"
    This reverts commit a5e0318c101c78f7e4fa00433257c85d38c5f8af.

--- 2make_image_to_gif.sh
+++ 2make_image_to_gif.sh
@@ -3,5 +3,5 @@ giffps=${frame}
 size=480
 today=`date +%Y%m%d%H%M%S`
 
-".\ffmpeg.exe" -f image2 -framerate ${frame} -i ".\make\Base%6d.jpg" -vf "scale=$size:-1:flags=lanczos,split[s0][s1];[s0]palettegen=stats_mode=diff[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3:diff_mode=rectangle" -loop 0 -r ${giffps} ./${today}.gif
+".\ffmpeg.exe" -f image2 -framerate ${frame} -i ".\make\Base%6d.jpg" -vf "scale=$size:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 -r ${giffps} ./${today}.gif
 #read -n 1 -p "Press any key to continue..."
