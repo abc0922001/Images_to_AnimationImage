f141b8d8bcbbef2c953db9abd801338411b827a0
abc0922001 <abc0922001@hotmail.com>
2022-01-06 22:30:28 +0800

    FFmpeg : Optimize parameter

--- 2make_image_to_gif.sh
+++ 2make_image_to_gif.sh
@@ -9,5 +9,14 @@ frame=30
 giffps=${frame}
 today=$(date +%Y%m%d%H%M%S)
 
-".\ffmpeg.exe" -f image2 -framerate ${frame} -i ".\make\Base%6d.jpg" -vf "crop=${w_cut}:${h_cut}:${x_cutpoint}:${y_cutpoint},scale=${size}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3" -loop 0 -r ${giffps} ./"${today}".gif
+
+#====ffmpeg parameter====
+sourceName=".\make\Base%6d.jpg"
+cutParameter=${w_cut}:${h_cut}:${x_cutpoint}:${y_cutpoint}
+outputSize=${size}:-1
+qualityParameter="lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3"
+outputName=./"${today}".gif
+#========================
+
+".\ffmpeg.exe" -f image2 -framerate ${frame} -i ${sourceName} -vf "crop=${cutParameter},scale=${outputSize}:flags=${qualityParameter}" -loop 0 -r ${giffps} ${outputName}
 #read -n 1 -p "Press any key to continue..."
