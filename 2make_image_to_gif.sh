214954ecaa7e55af9fab56fcb4eb76e8836430e1
abc0922001 <abc0922001@hotmail.com>
2022-04-11 20:53:22 +0800

    file name add Remark
    比較好搜尋

--- 2make_image_to_gif.sh
+++ 2make_image_to_gif.sh
@@ -5,18 +5,18 @@ w_cut=iw
 h_cut=ih
 x_cutpoint=0
 y_cutpoint=0
+Remark=""
 gifspeed=1
 frame=30
 giffps=${frame}
 today=$(date +%Y%m%d%H%M%S)
-
 
 #====ffmpeg parameter====
 sourceName=".\make\Base%6d.jpg"
 cutParameter=${w_cut}:${h_cut}:${x_cutpoint}:${y_cutpoint}
 outputSize=${size}:-1
 qualityParameter="lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3"
-outputName=./"${today}".gif
+outputName=./"${today}${Remark}".gif
 #========================
 
 ".\ffmpeg.exe" -f image2 -framerate ${frame}*${gifspeed} -i ${sourceName} -vf "crop=${cutParameter},scale=${outputSize}:flags=${qualityParameter}" -loop 0 -r ${giffps} "${outputName}"
