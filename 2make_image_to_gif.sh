557ebb1997a2b6114f4d7eba26a6626797b29155
abc0922001 <abc0922001@hotmail.com>
2022-08-08 23:32:09 +0800

    make_image_to_gif.sh : auto Size
    透過計算 gif 的影片長度和長寬
    盡量讓 gif 的檔案大小在 gif_size 左右

--- 2make_image_to_gif.sh
+++ 2make_image_to_gif.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-size=480
-w_cut=iw
-h_cut=ih
+z_contants=3.74
+w_cut=1920
+h_cut=1080
 x_cutpoint=0
 y_cutpoint=0
 Remark=""
+gif_size=6
 gifspeed=1
 frame=30
 giffps=${frame}
+
+image_count=$(find ".\make" -maxdepth 1 -type f -printf . | wc -c)
+gif_length=$(echo "$image_count" "$frame" "$gifspeed" | awk '{print $1/$2/$3}')
+aspect_ratio=$(echo "$w_cut" "$h_cut" | awk '{print $1/$2}')
+tmp=$(echo "$gif_size" "$aspect_ratio" "$giffps" "$z_contants" "$gif_length" | awk '{print $1*8*$2/$3/$4/$5}')
+square_root=$(echo "$tmp" | awk '{print sqrt($1)}')
+size=$(echo "$square_root" | awk '{print int($1*1024+0.5)}')
+echo "size of $size"
 today=$(date +%Y%m%d%H%M%S)
 
 #====ffmpeg parameter====
