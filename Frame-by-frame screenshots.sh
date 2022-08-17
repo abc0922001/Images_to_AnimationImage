0f29b7c91b2d04d30c2b4729950bf00156405051
abc0922001 <abc0922001@hotmail.com>
2022-08-17 20:24:43 +0800

    Frame-by-frame screenshots.sh optimize

--- Frame-by-frame screenshots.sh
+++ Frame-by-frame screenshots.sh
@@ -5,6 +5,7 @@ filename=""
 startTime="00:00:00"
 cutTime=3
 imageScale=1920:1080
+videoFormat=mp4
 #========================
 
-".\ffmpeg.exe" -ss "$startTime" -i "$filename" -t "$cutTime" -vf scale="$imageScale" -vsync 0 -f image2 ".\make\image-%03d.png"
+".\ffmpeg.exe" -ss "$startTime" -i "$filename"."$videoFormat" -t "$cutTime" -vf scale="$imageScale" -compression_level 0 -vsync 0 -f image2 ".\make\image-%03d.png"
