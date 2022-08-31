e6cef61bb1bd87fc3b802714df67728eaa4bf38a
abc0922001 <abc0922001@hotmail.com>
2022-08-31 21:09:31 +0800

    2make_image_to_gif : outputName add size and gifspeed

--- 2make_image_to_gif.sh
+++ 2make_image_to_gif.sh
@@ -30,7 +30,7 @@ sourceName=".\make\Base%6d.png"
 cutParameter=${w_cut}:${h_cut}:${x_cutpoint}:${y_cutpoint}
 outputSize=${size}:-1
 qualityParameter="lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3"
-outputName=./"${today}${Remark}".gif
+outputName=./"${today}${Remark}_${size}_${gifspeed}".gif
 #========================
 
 ".\ffmpeg.exe" -f image2 -framerate ${frame}*${gifspeed} -i ${sourceName} -vf "crop=${cutParameter},scale=${outputSize}:flags=${qualityParameter}" -loop 0 -r ${giffps} "${outputName}"
