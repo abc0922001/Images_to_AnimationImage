24f846c368b73c6a51b9ad224d63772c51743a57
abc0922001 <abc0922001@hotmail.com>
2022-09-19 21:54:53 +0800

    2make_image_to_gif : modify to scaleParameter

--- 2make_image_to_gif.sh
+++ 2make_image_to_gif.sh
@@ -28,10 +28,9 @@ today=$(date +%Y%m%d%H%M%S)
 #====ffmpeg parameter====
 sourceName=".\make\Base%6d.png"
 cutParameter=${w_cut}:${h_cut}:${x_cutpoint}:${y_cutpoint}
-outputSize=${size}:-1
-qualityParameter="lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3"
+scaleParameter="${size}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3"
 outputName=./"${today}${Remark}_${size}_${gifspeed}".gif
 #========================
 
-".\ffmpeg.exe" -f image2 -framerate ${frame}*${gifspeed} -i ${sourceName} -vf "crop=${cutParameter},scale=${outputSize}:flags=${qualityParameter}" -loop 0 -r ${giffps} "${outputName}"
+".\ffmpeg.exe" -f image2 -framerate ${frame}*${gifspeed} -i ${sourceName} -vf "crop=${cutParameter},scale=${scaleParameter}" -loop 0 -r ${giffps} "${outputName}"
 #read -n 1 -p "Press any key to continue..."
