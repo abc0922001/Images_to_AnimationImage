2d09d5e5f0fa02b8c212b9137b42c2a0b45afa17
abc0922001 <abc0922001@hotmail.com>
2023-11-14 19:23:12 +0800

    1rename.sh optimize

--- 1rename.sh
+++ 1rename.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cd make
+if [ ! -d "make" ]; then
+  echo "目錄 'make' 不存在。"
+  exit 1
+fi
+
+cd make || exit
 x=0
 
 for f in *.png; do
-  new_name=$(printf "Base%06d.png" $x)
+  new_name=$(printf "Base%06d.png" "$x")
   mv -- "$f" "$new_name"
   x=$((x+1))
 done
