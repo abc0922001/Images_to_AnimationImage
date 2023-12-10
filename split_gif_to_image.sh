93f0aa3b16bf4995a0cb8a779d96f9623c645236
abc0922001 <abc0922001@hotmail.com>
2023-12-10 19:49:11 +0800

    split_gif_to_image.sh code refactoring

--- split_gif_to_image.sh
+++ split_gif_to_image.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-input_gif="your_input_gif_file.gif"  # 更換為您的GIF文件名
-output_folder=".\make"  # 輸出PNG文件的文件夾
+# 定義輸入和輸出路徑
+input_gif_file="your_input_gif_file.gif"  # 更換為您的 GIF 文件名
+output_folder="./make"  # 輸出 PNG 文件的文件夾
+
+# 檢查輸入文件是否存在
+if [ ! -f "$input_gif_file" ]; then
+    echo "錯誤：GIF 文件不存在"
+    exit 1
+fi
 
 # 創建輸出文件夾（如果不存在）
 mkdir -p "${output_folder}"
 
-# 使用ffmpeg將GIF拆分為PNG文件
-ffmpeg -i "${input_gif}" -hide_banner -loglevel error "${output_folder}/Base%06d.png"
-
-echo "GIF 已成功拆分為 PNG 文件"
+# 使用 ffmpeg 將 GIF 拆分為 PNG 文件
+if ffmpeg -i "${input_gif_file}" -hide_banner -loglevel error "${output_folder}/Base%06d.png"; then
+    echo "GIF 已成功拆分為 PNG 文件"
+else
+    echo "錯誤：無法將 GIF 拆分為 PNG 文件"
+fi
