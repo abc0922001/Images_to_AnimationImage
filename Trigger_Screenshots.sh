d4e355fc3588a4b52996b75f751b0cd97568d3e1
abc0922001 <abc0922001@hotmail.com>
2024-06-10 01:57:44 +0800

    Trigger_Screenshots modify Path

--- Trigger_Screenshots.sh
+++ Trigger_Screenshots.sh
@@ -2,5 +2,5 @@
 
 start "clear_make_file.bat"
 sleep 3
-sh "D:\Videos\annie\hitomi_downloaded_bili\Frame-by-frame screenshots.sh"
+sh "D:\Videos\Annie\ytdlp_download\Frame-by-frame screenshots.sh"
 #read -n 1 -p "Press any key to continue..."
